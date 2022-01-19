e13e8d7ceea0f9c444d368d573d1a325b446e132
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2022-01-19 14:51:50 +0100

    ajout carousel essaie

--- assets/components/Carousel.js
+++ assets/components/Carousel.js
@@ -8,9 +8,39 @@ const Carousel = () => {
             <h1>Carousel</h1>
 
 
+            <div className="sneakers360">
+                <div className="slide">
+                    <div className="slidesauto" id="image2">
+
+
+                        <div className="mySlides fade">
+                            <div className="numbertext">1</div>
+                            <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp"/>
+                            <div className="text">???</div>
+                        </div>
+
+                        <div className="mySlides fade">
+                            <div className="numbertext">2</div>
+                            <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img02.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp"/>
+                            <div className="text">????</div>
+                        </div>
+
+
+                        <div className="mySlides fade">
+                            <div className="numbertext">3</div>
+                            <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img03.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp"/>
+                            <div className="text">????</div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
 
 
         </div>
+
     );
 };
 
