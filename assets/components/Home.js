a4a8bac3ae4afa8bce866d0910a0d2981501aba7
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2022-01-19 10:47:44 +0100

    ajout card page accueil

--- assets/components/Home.js
+++ assets/components/Home.js
@@ -1,23 +1,99 @@
 import React, {Component} from 'react';
+import "../styles/home.css";
+import "../js/home"
 
 class Home extends Component {
 
     render() {
+
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="collapse navbar-collapse" id="navbarText">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <p className="btn btn-danger">Hello</p>
-                            </li>
 
-                            <li className="nav-item">
-                                <p>Hello</p>
-                            </li>
-                        </ul>
+        <div className="newsneakers" >
+
+            {/* début 1 card sneakers page acceuil */}
+
+                <div className="card sneakerscard" >
+                    <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Jordan 1 Retro High OG</h5>
+                            <p className="card-text">Prix : 200$</p>
+                        </div>
+                    <div className="card-bodyy">
+                        <a href="#" className="card-link">Voir</a>
                     </div>
-                </nav>
+                </div>
+
+            {/* fin 1 card sneakers page acceuil */}
+
+
+            <div className="card sneakerscard" >
+                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
+                <div className="card-body">
+                    <h5 className="card-title">Jordan 1 Retro High OG</h5>
+                    <p className="card-text">Prix : 200$</p>
+                </div>
+                <div className="card-bodyy">
+                    <a href="#" className="card-link">Voir</a>
+                </div>
+            </div>
+
+
+
+            <div className="card sneakerscard" >
+                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
+                <div className="card-body">
+                    <h5 className="card-title">Jordan 1 Retro High OG</h5>
+                    <p className="card-text">Prix : 200$</p>
+                </div>
+                <div className="card-bodyy">
+                    <a href="#" className="card-link">Voir</a>
+                </div>
+            </div>
+
+
+
+            <div className="card sneakerscard" >
+                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
+                <div className="card-body">
+                    <h5 className="card-title">Jordan 1 Retro High OG</h5>
+                    <p className="card-text">Prix : 200$</p>
+                </div>
+                <div className="card-bodyy">
+                    <a href="#" className="card-link">Voir</a>
+                </div>
+            </div>
+
+
+
+
+            <div className="card sneakerscard" >
+                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
+                <div className="card-body">
+                    <h5 className="card-title">Jordan 1 Retro High OG</h5>
+                    <p className="card-text">Prix : 200$</p>
+                </div>
+                <div className="card-bodyy">
+                    <a href="#" className="card-link">Voir</a>
+                </div>
+            </div>
+
+
+
+            <div className="card sneakerscard" >
+                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
+                <div className="card-body">
+                    <h5 className="card-title">Jordan 1 Retro High OG</h5>
+                    <p className="card-text">Prix : 200$</p>
+                </div>
+                <div className="card-bodyy">
+                    <a href="#" className="card-link">Voir</a>
+                </div>
+            </div>
+
+
+        </div>
+
             </div>
         )
     }
