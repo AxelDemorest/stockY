5b2288545a0b9219bc6066cacbb16ab392dd5af9
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2022-01-20 13:38:53 +0100

    ajout page connexion

--- assets/router.js
+++ assets/router.js
@@ -3,6 +3,8 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Catalog from "./pages/Catalog";
 import Products from "./pages/Products";
+import Connexion from "./pages/Connexion";
+import Inscription from "./pages/Inscription"
 
 const Router = () => {
     return (
@@ -11,6 +13,8 @@ const Router = () => {
             <Route path="/login" element={<Home />}/>
             <Route path="/catalogue" element={<Catalog />}/>
             <Route path="/products" element={<Products />}/>
+            <Route path="/connexion" element={<Connexion />}/>
+            <Route path="/inscription" element={<Inscription />}/>
 
         </Routes>
     );
