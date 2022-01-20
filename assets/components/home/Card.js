77bcb55bc6036dbc29527b79a0ce0eea5936222b
AxelDemorest <axel.demorest@gmail.com>
2022-01-20 23:53:24 +0100

    Dynamic result list of products in home page

---
+++ assets/components/home/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = ({ product }) => {
+    return (
+        <div className="card sneakerscard" >
+            {/*https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp*/}
+            <img src={product.picture} className="card-img-top" alt="img1"/>
+            <div className="card-body">
+                <p className="card-title mb-1">{product.name}</p>
+                <p className="card-text category-card text-secondary">Catégorie</p>
+                <p className="card-text mb-0">€{product.price}</p>
+            </div>
+                <div className="d-flex justify-content-center">
+                    <a href="#" className="card-bottom-link">Voir plus</a>
+                </div>
+        </div>
+    );
+};
+
+export default Card;
--- assets/pages/Home.js
+++ assets/pages/Home.js
@@ -1,93 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "../styles/pages/home.css";
 import Carousel from "../components/Carousel";
+import Card from "../components/home/Card";
+import axios from "axios";
 
 const Home = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/products/6')
+            .then(function (response) {
+                setProducts(response.data)
+            })
+    }, [])
+
     return (
         <div className="home">
             <Carousel />
             <h2 className="title-category">Notre sélection</h2>
             <div className="newsneakers" >
-            {/* début 1 card sneakers page acceuil */}
-
-                <div className="card sneakerscard" >
-                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
-                <div className="card-body">
-                <h5 className="card-title">Jordan 1 Retro High OG</h5>
-                <p className="card-text">Prix : 200$</p>
-                </div>
-                <div className="card-bodyy">
-                <a href="#" className="card-link">Voir</a>
-                </div>
-                </div>
-
-            {/* fin 1 card sneakers page acceuil */}
-
-
-                <div className="card sneakerscard" >
-                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
-                <div className="card-body">
-                <h5 className="card-title">Jordan 1 Retro High OG</h5>
-                <p className="card-text">Prix : 200$</p>
-                </div>
-                <div className="card-bodyy">
-                <a href="#" className="card-link">Voir</a>
-                </div>
-                </div>
-
-
-
-                <div className="card sneakerscard" >
-                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
-                <div className="card-body">
-                <h5 className="card-title">Jordan 1 Retro High OG</h5>
-                <p className="card-text">Prix : 200$</p>
-                </div>
-                <div className="card-bodyy">
-                <a href="#" className="card-link">Voir</a>
-                </div>
-                </div>
-
-
-
-                <div className="card sneakerscard" >
-                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
-                <div className="card-body">
-                <h5 className="card-title">Jordan 1 Retro High OG</h5>
-                <p className="card-text">Prix : 200$</p>
-                </div>
-                <div className="card-bodyy">
-                <a href="#" className="card-link">Voir</a>
-                </div>
-                </div>
-
-
-
-
-                <div className="card sneakerscard" >
-                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
-                <div className="card-body">
-                <h5 className="card-title">Jordan 1 Retro High OG</h5>
-                <p className="card-text">Prix : 200$</p>
-                </div>
-                <div className="card-bodyy">
-                <a href="#" className="card-link">Voir</a>
-                </div>
-                </div>
-
-
-
-                <div className="card sneakerscard" >
-                <img src="https://images.stockx.com/360/Air-Jordan-1-Retro-High-OG-Bred-Patent/Images/Air-Jordan-1-Retro-High-OG-Bred-Patent/Lv2/img01.jpg?auto=compress&w=480&q=90&dpr=2&updated_at=1633542046&h=320&fm=webp" className="card-img-top" alt="img1"/>
-                <div className="card-body">
-                <h5 className="card-title">Jordan 1 Retro High OG</h5>
-                <p className="card-text">Prix : 200$</p>
-                </div>
-                <div className="card-bodyy">
-                <a href="#" className="card-link">Voir</a>
-                </div>
-                </div>
-
+                {products.map((product) => (
+                    <Card product={product} />
+                ))}
             </div>
 
             <h2 className="title-category">Les dernières sorties</h2>
