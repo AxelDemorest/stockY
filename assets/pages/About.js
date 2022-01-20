ecec9e1f88b8238bdaec28af83f9826347ab87a6
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2022-01-20 15:07:28 +0100

    update page about icon

--- assets/pages/About.js
+++ assets/pages/About.js
@@ -19,9 +19,11 @@ const About = () => {
 
                 <div className="card cardabout2" >
 
-                    <img className={"imgabout"} src="https://stockx.com/about/wp-content/uploads/2021/04/verified-authentic2021-400x230.png" />
+                    <img className={"imgabout2"} src="https://stockx.com/about/wp-content/uploads/2021/04/transparent-pricing2021.png" />
                     <div className="card-body">
-                        <p className="card-text">Air Jordan 6 Low </p>
+                        <p className="card-title">Transparent Pricing </p>
+                        <p className="card-textabout">Our real-time marketplace works just like the stock market –  allowing you to buy and sell the most coveted items at their true market price.  </p>
+
 
                     </div>
                 </div>
@@ -29,9 +31,11 @@ const About = () => {
 
                 <div className="card cardabout2" >
 
-                    <img className={"imgabout"} src="https://stockx.com/about/wp-content/uploads/2021/04/verified-authentic2021-400x230.png" />
+                    <img className={"imgabout3"} src="https://stockx.com/about/wp-content/uploads/2021/04/global-access2021.png" />
                     <div className="card-body">
-                        <p className="card-text">Air Jordan 6 Low </p>
+                        <p className="card-title">Global Access </p>
+                        <p className="card-textabout">Whether it’s pre-release, regionally limited, or “sold out” – our millions of customers from over 200+ countries allow you to easily secure those hard-to-find, coveted items.  </p>
+
 
                     </div>
                 </div>
