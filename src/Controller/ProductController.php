1bc0627ecc31f5c8ccc7461aba1eec547d137071
AxelDemorest <axel.demorest@gmail.com>
2022-01-21 11:14:09 +0100

    Fix bug dynamic result in home page (last releases)

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/api/products/{limit}", name="products")
+     */
+    public function getProducts(SerializerInterface $serializer, ManagerRegistry $doctrine, Request $request, $limit = null): Response
+    {
+        $param = $request->query->get('orderBy');
+        if($param) {
+            $orderBy = array('id' => "DESC");
+        } else $orderBy = array();
+        $data = $doctrine->getRepository(Product::class)->findBy(array(), $orderBy, $limit);
+        return new Response($serializer->serialize($data, JsonEncoder::FORMAT));
+    }
+}
