079fec664fcf3164a7259ee0c3f328eb4ee7e996
alexismajchrzak <72200961+alexismajchrzak@users.noreply.github.com>
2022-01-27 11:32:31 +0100

    réglage conflit sur les input de chaque pages

--- assets/components/Account/accountcomponent.js
+++ assets/components/Account/accountcomponent.js
@@ -5,28 +5,31 @@ const Accountcomponent = () => {
     return (
         <div>
 
-            <div className="mb-4">
+        <div className="formaccount" >
+            <div className="mb-3 ">
                 <label htmlFor="formGroupExampleInput" className="form-label">Prénom :</label>
                 <input type="text" className="form-controll" id="formGroupExampleInput"
                        placeholder="Alexis "/>
             </div>
-            <div className="mb-4">
+            <div className="mb-3">
                 <label htmlFor="formGroupExampleInput2" className="form-label">Nom :</label>
                 <input type="text" className="form-controll" id="formGroupExampleInput2"
                        placeholder="Majchrzak "/>
             </div>
-            <div className="mb-4">
+            <div className="mb-3">
                 <label htmlFor="formGroupExampleInput2" className="form-label">Email :</label>
                 <input type="email" className="form-controll" id="formGroupExampleInput2"
                        placeholder="alexis.stockY@gmail.com "/>
             </div>
-            <div className="mb-4">
+            <div className="mb-3">
                 <label htmlFor="formGroupExampleInput2" className="form-label">Téléphone :</label>
                 <input type="email" className="form-controll" id="formGroupExampleInput2"
                        placeholder="+33 00 00 00 00  "/>
             </div>
 
             <button className={"boutonaccount"} > Confirmer </button>
+
+        </div>
 
         </div>
     );
